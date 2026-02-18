d9b6c1dfd346e7f5bd577540b4e291ac70806a80
neoayus <ayus43@outlook.com>
2026-02-18 23:32:24 +0530

    chore: learn basics of pykakasi

--- learnPYKAKASI.py
+++ learnPYKAKASI.py
@@ -1,1 +1,19 @@
 # IMPORT PYKAKASI 
+import pykakasi 
+
+kks = pykakasi.kakasi()
+text = '（リコーダーの演奏）' # kanji subs 
+
+# hepburn = hiragana
+
+result = kks.convert(text);
+# print(result[3]['hepburn']);
+
+for item in result: 
+    print(item['hepburn']);
+    # print("{}: kana '{}', hiragana '{}', romaji: '{}'".format(
+    #     item['orig'], 
+    #     item['kana'], 
+    #     item['hira'], 
+    #     item['hepburn']
+    # ))
