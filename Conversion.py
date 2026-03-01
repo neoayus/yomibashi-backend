a1e0768b358b0d102897a1e14b049c4d7f526fef
neoayus <ayus43@outlook.com>
2026-03-01 17:50:21 +0530

    feat: modify this script to work as a function

--- Conversion.py
+++ Conversion.py
@@ -1,34 +1,35 @@
-import sys        # library: for commandline input 
 import srt        # library used to perform operatons on subtitles.
 import pykakasi   # library used for conversion. 
 
-# Input file from user using cmd argument: 
-inputFile = sys.argv[1]; 
-output_file = inputFile.split('.')[0] + '_romaji' + ".srt"
 
-# Read File Using Srt Library : 
-with open(inputFile, "r", encoding="utf-8") as subtitle_file:
-  content = subtitle_file.read()  
+def convert_srt(input_file):
+  output_file = input_file.split('.')[0] + '_romaji' + ".srt"
+
+  # Read File Using Srt Library : 
+  with open(input_file, "r", encoding="utf-8") as f:
+    content = f.read()  
+
+  # Parse Subtitles into Objects 
+  subtitles = list(srt.parse(content))
+
+  # Setup Pykakasi Converter 
+  kks = pykakasi.kakasi()
+
+  for sub in subtitles: 
+    # read subtitle content 
+    original_text = sub.content 
+
+    # perform conversion: japanese --> romaji: 
+    converted = kks.convert(original_text)  
+    romaji_text = " ".join(item['hepburn'] for item in converted)
+    
+    # replace subtitle text 
+    sub.content = romaji_text
   
-# Parse Subtitles into Objects 
-subtitles = list(srt.parse(content))
+  # write to new file: 
+  new_content = srt.compose(subtitles)
 
-# Setup Pykakasi Converter 
-kks = pykakasi.kakasi()
-
-for sub in subtitles: 
-  # read subtitle content 
-  original_text = sub.content 
-
-  # perform conversion: japanese --> romaji: 
-  converted = kks.convert(original_text)  
-  romaji_text = " ".join(item['hepburn'] for item in converted)
-  
-  # replace subtitle text 
-  sub.content = romaji_text
-  
-# write to new file: 
-new_content = srt.compose(subtitles)
-
-with open(output_file, "x", encoding="utf-8") as output_file: 
-  output_file.write(new_content)
+  with open(output_file, "w", encoding="utf-8") as f: 
+    f.write(new_content)
+    
+  return output_file
