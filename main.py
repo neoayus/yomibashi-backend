dacec525c4e3a6a94f2fc5a5ceb85e809c1e54f1
neoayus <ayus43@outlook.com>
2026-03-01 17:58:40 +0530

    chore: write API

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI, UploadFile, File
+import shutil 
+
+# import conversion function 
+from Conversion import convert_srt 
 
 app = FastAPI()
 
+# upload file 
+# @app.post("/upload")
+# def upload_file(file: UploadFile = File(...)):
+#     return {"filename": file.filename}
+  
 @app.post("/upload")
-def upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+async def upload(file: UploadFile = File(...)):
+
+  # save file  
+  with open(file.filename, "wb") as buffer: 
+    shutil.copyfileobj(file.file, buffer)
+
+  # convert file 
+  new_file = convert_srt(file.filename)
+  
+  # return new file 
+  return{
+    "saved": file.filename, 
+    "converted": new_file
+  }
